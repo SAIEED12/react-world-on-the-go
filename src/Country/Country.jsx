ed1d46fa9e3e4e74ffcd40dbdf8d995a81ff40f0
SAIEED12 <abusaieed0@gmail.com>
2026-03-15 11:31:07 +0600

    Toggle Button added

---
+++ src/Country/Country.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import "./Country.css"
+
+const Country = ({country}) => {
+    const [visited, setVisited] = useState(false)
+
+    const handleVisited = ()=>{
+        // if(visited){
+        //     setVisited(false)
+        // }
+        // else{
+        //     setVisited(true)
+        // }
+
+        // Ternary
+        // setVisited(visited ? false : true)
+
+        setVisited(!visited)
+    }
+    return (
+        <div className={`country ${visited && 'country-visited'}`}>
+            <img src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
+            <h3>Name: {country.name.common}</h3>
+            <p>Population: {country.population.population}</p>
+            <p>Area: {country.area.area} {country.area.area > 300000 ? "Big Country" : "Small Country"}</p>
+            <button onClick={handleVisited}>
+                {visited ? "Visited" : "Not Visited"}
+            </button>
+        </div>
+    );
+};
+
+export default Country;
